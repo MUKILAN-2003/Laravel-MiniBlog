92c028feec667b0a5199eafb3036cb34f37f7cfa
S.MUKILAN <65155327+MUKILAN-2003@users.noreply.github.com>
2021-07-27 18:45:53 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'MiniBlog') }}</title>
+    <title>'MiniBlog'</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -31,7 +31,7 @@
         <nav class="navbar navbar-expand-md navbar-light shadow-sm top-navbar">
             <div class="container navbar-container">
                 <a style="font-size: 34px;color: #f60;" class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'MiniBlog') }}
+                    MiniBlog
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
